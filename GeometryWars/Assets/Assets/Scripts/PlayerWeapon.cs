a312921bc4e36b0a1a3cb8fe228562f8e3efcac9
Zockerjimmy <alexander_schmidt2@hotmail.de>
2017-05-24 22:11:46 +0200

    First shooting (code only)

---
+++ GeometryWars/Assets/Assets/Scripts/PlayerWeapon.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerWeapon : MonoBehaviour {
+
+    private Transform bulletSpawn;
+    public float fShootSpeed = 0.25f;
+    public bool bShot = false;
+
+	void Start ()
+    {
+        bulletSpawn = GameObject.Find("BulletSpawn").transform;
+	}
+	
+	void Update ()
+    {
+
+	}
+
+    public void Shoot()
+    {
+
+    }
+}
